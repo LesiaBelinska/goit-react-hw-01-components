2ad1774f1af1fbd840aa20d1c1cbe9e358cf3fa9
LesiaBelinska <lm.kudak@gmail.com>
2022-04-13 22:13:35 +0300

    Profile_done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,21 @@
 import { Profile } from './Profile/Profile.jsx';
 
+import user from '../data/user.json';
+
 import s from './App.module.css';
 
 
 export const App = () => {
   return (
     <div className={s.App}>
-      <Profile />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      
     </div>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,24 +1,45 @@
-import { ProfileDescription } from "components/ProfileDescription/ProfileDescription.jsx";
-import { StatList } from "components/StatList/StatList.jsx";
-
-import user from '../../data/user.json';
+import PropTypes from 'prop-types';
 
 import s from './Profile.module.css'
 
-export const Profile = () => {
+export const Profile = ({ username, tag, location, avatar, stats }) => {
  
     return <div className={s.profile}>
-        <ProfileDescription
-            username={user.username}
-            tag={user.tag}
-            location={user.location}
-            avatar ={user.avatar}
-        />
-        <StatList
-            followers={user.stats.followers}
-            views={user.stats.views}
-            likes={user.stats.likes}
-        />
-        
+        <div className={s.description}>
+    <img
+        src={avatar}
+        alt={username}
+        className={s.avatar}
+    />
+      <p className={s.name}>{username}</p>
+    <p className={s.tag}>@{tag}a</p>
+        <p className={s.location}>{location}</p>
     </div>
+  <ul className={s.stats}>
+    <li className={s.item}>
+      <span className={s.label}>Followers</span>
+        <span className={s.quantity}>{stats.followers}</span>
+    </li>
+    <li className={s.item}>
+      <span className={s.label}>Views</span>
+        <span className={s.quantity}>{stats.views}</span>
+    </li>
+    <li className={s.item}>
+      <span className={s.label}>Likes</span>
+        <span className={s.quantity}>{stats.likes}</span>
+    </li>
+  </ul> 
+    </div>
+}
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired
+  }),
 }
