7c2425c9eed938bdb21b0134f3fb26fcbc7e3ec6
LesiaBelinska <lm.kudak@gmail.com>
2022-04-14 00:22:39 +0300

    create Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import { Profile } from './Profile/Profile.jsx';
+import { Statistics } from './Statistics/Statistics.jsx';
 
 import user from '../data/user.json';
+import data from '../data/data.json';
 
 import s from './App.module.css';
 
@@ -15,7 +17,9 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      
+      <Statistics
+        title="Upload stats"
+        stats={data} />
     </div>
   );
 };
